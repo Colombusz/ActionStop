8f94c71a84b92cdebd42116a6708ba1600be0697
Protatooo <133950548+LacaoProtatooo@users.noreply.github.com>
2024-11-05 15:01:32 +0800

    tulog muna

--- backend/app.js
+++ backend/app.js
@@ -2,18 +2,23 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import figurineRoutes from "./routes/figurine_route.js";
+import manufacturerRoutes from "./routes/manufacturer_route.js";
+import promoRoutes from "./routes/promo_route.js";
+
 const app = express();
 
 app.use(express.json({limit:'50mb'}));
 app.use(express.urlencoded({extended:true, limit:'50mb'}));
 
 // Middleware
-app.use(cookieParser());
-app.use(cors());
+// app.use(cookieParser());
+// app.use(cors());
 
 // Router Connection
-import figurineRoutes from "./routes/figurine_route.js";
 app.use("/api/figurines", figurineRoutes);
+app.use("/api/manufacturers", manufacturerRoutes);
+app.use("/api/promos", promoRoutes);
 
 // Export the app
 export default app;
