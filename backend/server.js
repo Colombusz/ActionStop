0ed490b22defa819b19a400dfb00f58b6e651097
ELI <Eliseo.lim12@gmail.com>
2024-11-06 12:10:49 +0800

    SINGLE SERVER

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,20 @@ import app from "./app.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 5000
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 app.use(express.json());  // Allows to accept JSON data in the req.body
 
 console.log(process.env.MONGO_URI);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res)=>{
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    })
+
+}
 
 app.listen(PORT, () => {
     connectDB();
